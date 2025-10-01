f036d69dec51869ffe936a0c982957f0330afac6
Jeevan Pukalenthi <jeepukal@gmail.com>
2025-10-01 15:56:22 +0530

    made changes in sum2.java

--- sum2.java
+++ sum2.java
@@ -2,6 +2,6 @@ public class sum2 {
     public static void main(String[]args){
     int a=5;
     int b=10;
-    System.out.println("THE SUM IS "+(a+b));
+    System.out.println("THE SUM IS  THIS"+(a+b));
 }}
 
