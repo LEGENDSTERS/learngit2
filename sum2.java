a2db96ccf89001b09920753a5912e4fd24139c70
Jeevan Pukalenthi <jeepukal@gmail.com>
2025-10-01 15:55:15 +0530

    added message in sum2.java

--- sum2.java
+++ sum2.java
@@ -1,7 +1,7 @@
 public class sum2 {
-    public static void main(String[]args)
+    public static void main(String[]args){
     int a=5;
     int b=10;
-    System.out.println(a+b);
-    
-}
+    System.out.println("THE SUM IS "+(a+b));
+}}
+
